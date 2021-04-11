7bc71c0e472c266b18f1526caca14eea21b8caa7
gifnksm <makoto.nksm+github@gmail.com>
2021-04-11 23:16:05 +0900

    Trim dependencies

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,9 @@
 #![allow(unused_imports)]
 
 pub(crate) use color_eyre::eyre::{bail, ensure, eyre, Error, Result, WrapErr};
-pub(crate) use futures::prelude::*;
+pub(crate) use futures_util::{
+    future::{self, Future, FutureExt as _, TryFutureExt as _},
+    stream::StreamExt as _,
+};
 pub(crate) use tracing::{debug, info, info_span, trace, warn, Instrument};
 pub(crate) use tracing_subscriber::prelude::*;
